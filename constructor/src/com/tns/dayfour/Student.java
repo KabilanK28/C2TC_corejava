e4a0d66a92cb06339424556b3c831cb6d9523f1e
karan <karan@kabilan001>
2024-07-25 04:32:27 +0530

    hello

---
+++ constructor/src/com/tns/dayfour/Student.java
@@ -0,0 +1,16 @@
+package com.tns.dayfour;
+
+public class Student {
+       Student() {
+    	   System.out.println("Student");
+       }
+       Student(int a){
+    	   System.out.println(+a);
+       }
+       Student(float b){
+    	   System.out.println(+b);
+       }
+       Student(int a,float b){
+    	   System.out.println(+a +b);
+       }
+}
---
+++ constructor/src/com/tns/dayfour/Students.java
@@ -0,0 +1,12 @@
+package com.tns.dayfour;
+
+public class Students {
+
+	public static void main(String[] args) {
+		Student a=new Student();
+		Student a1=new Student(1);
+        Student a2=new Student(2);
+        Student b=new Student(1f);
+	}
+
+}
